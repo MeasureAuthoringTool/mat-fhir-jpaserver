d7afe82c0f69e4eec3323e78a3336612e87b70c0
carson.day <carson.day@semanticbits.com>
2020-04-02 13:38:45 -0400

    Added env var overrides for db_url,db_username,db_password,server url.

--- hapi-fhir-jpaserver-starter/src/main/java/ca/uhn/fhir/jpa/starter/HapiProperties.java
+++ hapi-fhir-jpaserver-starter/src/main/java/ca/uhn/fhir/jpa/starter/HapiProperties.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 import static org.apache.commons.lang3.StringUtils.*;
 
 public class HapiProperties {
-  static final String ENABLE_INDEX_MISSING_FIELDS = "enable_index_missing_fields";
+    static final String ENABLE_INDEX_MISSING_FIELDS = "enable_index_missing_fields";
     static final String AUTO_CREATE_PLACEHOLDER_REFERENCE_TARGETS = "auto_create_placeholder_reference_targets";
     static final String ENFORCE_REFERENTIAL_INTEGRITY_ON_WRITE = "enforce_referential_integrity_on_write";
     static final String ENFORCE_REFERENTIAL_INTEGRITY_ON_DELETE = "enforce_referential_integrity_on_delete";
@@ -31,8 +31,9 @@ public class HapiProperties {
     static final String ALLOW_MULTIPLE_DELETE = "allow_multiple_delete";
     static final String ALLOW_PLACEHOLDER_REFERENCES = "allow_placeholder_references";
     static final String REUSE_CACHED_SEARCH_RESULTS_MILLIS = "reuse_cached_search_results_millis";
-    static final String DATASOURCE_DRIVER = "datasource.driver";
+
     static final String DATASOURCE_MAX_POOL_SIZE = "datasource.max_pool_size";
+    static final String DATASOURCE_DRIVER = "datasource.driver";
     static final String DATASOURCE_PASSWORD = "datasource.password";
     static final String DATASOURCE_URL = "datasource.url";
     static final String DATASOURCE_USERNAME = "datasource.username";
@@ -69,8 +70,8 @@ public class HapiProperties {
     private static final String FILTER_SEARCH_ENABLED = "filter_search.enabled";
     private static final String GRAPHQL_ENABLED = "graphql.enabled";
     private static final String BULK_EXPORT_ENABLED = "bulk.export.enabled";
-  public static final String EXPIRE_SEARCH_RESULTS_AFTER_MINS = "retain_cached_searches_mins";
-  private static Properties ourProperties;
+    public static final String EXPIRE_SEARCH_RESULTS_AFTER_MINS = "retain_cached_searches_mins";
+    private static Properties ourProperties;
 
     public static boolean isElasticSearchEnabled() {
         return HapiProperties.getPropertyBoolean("elasticsearch.enabled", false);
@@ -82,6 +83,24 @@ public class HapiProperties {
     public static void forceReload() {
         ourProperties = null;
         getProperties();
+    }
+
+    private static void overrideWithEnvVars() {
+        envOverride(SERVER_ADDRESS,"HAPI_FHIR_URL_PUBLIC");
+        envOverride(DATASOURCE_URL,"HAPI_DB_URL");
+        envOverride(DATASOURCE_USERNAME,"HAPI_DB_USER");
+        envOverride(DATASOURCE_PASSWORD,"HAPI_DB_PASS");
+    }
+
+    private static void envOverride(String propertyName,String envVarName) {
+        String envValue = System.getenv(envVarName);
+        if (isNotBlank(envValue)) {
+            getProperties().setProperty(propertyName,envValue);
+        }
+    }
+
+    private static boolean isNotBlank(String s) {
+        return s != null && !s.trim().equals("");
     }
 
     /**
@@ -115,6 +134,7 @@ public class HapiProperties {
         if (ourProperties == null) {
             Properties properties = loadProperties();
             HapiProperties.ourProperties = properties;
+            overrideWithEnvVars();
         }
 
         return ourProperties;
@@ -122,19 +142,19 @@ public class HapiProperties {
 
     @NotNull
     private static Properties loadProperties() {
-            // Load the configurable properties file
+        // Load the configurable properties file
         Properties properties;
-            try (InputStream in = HapiProperties.class.getClassLoader().getResourceAsStream(HAPI_PROPERTIES)) {
+        try (InputStream in = HapiProperties.class.getClassLoader().getResourceAsStream(HAPI_PROPERTIES)) {
             properties = new Properties();
             properties.load(in);
-            } catch (Exception e) {
-                throw new ConfigurationException("Could not load HAPI properties", e);
-            }
+        } catch (Exception e) {
+            throw new ConfigurationException("Could not load HAPI properties", e);
+        }
 
-            Properties overrideProps = loadOverrideProperties();
-            if (overrideProps != null) {
-                properties.putAll(overrideProps);
-            }
+        Properties overrideProps = loadOverrideProperties();
+        if (overrideProps != null) {
+            properties.putAll(overrideProps);
+        }
         return properties;
     }
 
@@ -337,7 +357,7 @@ public class HapiProperties {
         return HapiProperties.getProperty(CORS_ALLOWED_ORIGIN, "*");
     }
 
-    public static  String getAllowedBundleTypes() {
+    public static String getAllowedBundleTypes() {
         return HapiProperties.getProperty(ALLOWED_BUNDLE_TYPES, "");
     }
 
@@ -407,10 +427,21 @@ public class HapiProperties {
     }
 
     // Defaults from https://javaee.github.io/javamail/docs/api/com/sun/mail/smtp/package-summary.html
-    public static Boolean getEmailAuth() { return HapiProperties.getBooleanProperty("email.auth", false); }
-    public static Boolean getEmailStartTlsEnable() { return HapiProperties.getBooleanProperty("email.starttls.enable", false); }
-    public static Boolean getEmailStartTlsRequired() { return HapiProperties.getBooleanProperty("email.starttls.required", false); }
-    public static Boolean getEmailQuitWait() { return HapiProperties.getBooleanProperty("email.quitwait", true); }
+    public static Boolean getEmailAuth() {
+        return HapiProperties.getBooleanProperty("email.auth", false);
+    }
+
+    public static Boolean getEmailStartTlsEnable() {
+        return HapiProperties.getBooleanProperty("email.starttls.enable", false);
+    }
+
+    public static Boolean getEmailStartTlsRequired() {
+        return HapiProperties.getBooleanProperty("email.starttls.required", false);
+    }
+
+    public static Boolean getEmailQuitWait() {
+        return HapiProperties.getBooleanProperty("email.quitwait", true);
+    }
 
     public static Long getReuseCachedSearchResultsMillis() {
         String value = HapiProperties.getProperty(REUSE_CACHED_SEARCH_RESULTS_MILLIS, "60000");
@@ -418,8 +449,8 @@ public class HapiProperties {
     }
 
     public static Long getExpireSearchResultsAfterMins() {
-      String value = HapiProperties.getProperty(EXPIRE_SEARCH_RESULTS_AFTER_MINS, "60");
-      return Long.valueOf(value);
+        String value = HapiProperties.getProperty(EXPIRE_SEARCH_RESULTS_AFTER_MINS, "60");
+        return Long.valueOf(value);
     }
 
     public static Boolean getCorsAllowedCredentials() {
@@ -439,24 +470,25 @@ public class HapiProperties {
     }
 
     public static boolean getGraphqlEnabled() {
-      return HapiProperties.getBooleanProperty(GRAPHQL_ENABLED, true);
+        return HapiProperties.getBooleanProperty(GRAPHQL_ENABLED, true);
     }
 
     public static boolean getEnforceReferentialIntegrityOnDelete() {
-      return HapiProperties.getBooleanProperty(ENFORCE_REFERENTIAL_INTEGRITY_ON_DELETE, true);
+        return HapiProperties.getBooleanProperty(ENFORCE_REFERENTIAL_INTEGRITY_ON_DELETE, true);
     }
 
     public static boolean getEnforceReferentialIntegrityOnWrite() {
-      return HapiProperties.getBooleanProperty(ENFORCE_REFERENTIAL_INTEGRITY_ON_WRITE, true);
+        return HapiProperties.getBooleanProperty(ENFORCE_REFERENTIAL_INTEGRITY_ON_WRITE, true);
     }
 
     public static boolean getAutoCreatePlaceholderReferenceTargets() {
-      return HapiProperties.getBooleanProperty(AUTO_CREATE_PLACEHOLDER_REFERENCE_TARGETS, true);
+        return HapiProperties.getBooleanProperty(AUTO_CREATE_PLACEHOLDER_REFERENCE_TARGETS, true);
     }
 
     public static boolean getEnableIndexMissingFields() {
-      return HapiProperties.getBooleanProperty(ENABLE_INDEX_MISSING_FIELDS, false);
+        return HapiProperties.getBooleanProperty(ENABLE_INDEX_MISSING_FIELDS, false);
     }
+
     private static boolean getPropertyBoolean(String thePropertyName, boolean theDefaultValue) {
         String value = getProperty(thePropertyName, Boolean.toString(theDefaultValue));
         return Boolean.parseBoolean(value);
